6723a05ab184b2cccd880aa4255dac6811433c9c
Thomas <393890@student.fontys.nl>
2020-09-01 13:22:36 +0200

    inventory and woodcutting added

---
+++ TDExplore/Assets/ItemPickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+
+    private PlayerInventory inventory;
+    public GameObject itemButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("player").GetComponent<PlayerInventory>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "player")
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    // inventory is not full
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
--- TDExplore/Assets/Scripts/TreeScript.cs
+++ TDExplore/Assets/Scripts/TreeScript.cs
@@ -4,31 +4,58 @@ using UnityEngine;
 
 public class TreeScript : MonoBehaviour
 {
-    public float currentHealth;
+    
     public float maxHealth;
+
+    public GameObject dropItem;
+   
+    private float currentHealth;
+    private bool isHitting;
+    
 
     public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isHitting)
+        {
+            currentHealth -= Time.deltaTime;
+            if (currentHealth <= 0)
+            {
+                isHitting = false;
+                animator.SetTrigger("treeIsCut");
+                
+                StartCoroutine("SpawnLog");
+                Destroy(gameObject, 2f);
+            }
+        }
     }
 
     public void Hitting()
     {
+        isHitting = true;
         Debug.Log("treehitto");
         animator.SetBool("isHitting", true);
     }
     public void StopHitting()
     {
+        isHitting = false;
         Debug.Log("tree not hitto");
         animator.SetBool("isHitting", false);
+    }
+
+    IEnumerator SpawnLog()
+    {
+        Debug.Log("waiting for log drop");
+        yield return new WaitForSeconds(1.9f);
+        Instantiate(dropItem, transform.position, Quaternion.identity);
+
     }
 }
